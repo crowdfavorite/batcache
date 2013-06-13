1d4be6ea728ea8460f1948d0000c1fc4623384fd
Jonathan Johnson <jon@crowdfavorite.com>
2013-06-13 15:35:52 -0600

    re-organization of manager plugin
      - Add "Flush Page Cache" to Admin Bar

--- batcache.php
+++ batcache.php
@@ -1,45 +1,88 @@
 <?php
 /*
-Plugin name: Batcache Manager
+Plugin name: Batcache Manager (CF Modified)
 Plugin URI: http://wordpress.org/extend/plugins/batcache/
 Description: This optional plugin improves Batcache.
-Author: Andy Skelton
+Author: Andy Skelton (CF Modified)
 Author URI: http://andyskelton.com/
-Version: 1.0
+Version: 0.1
 */
 
-// Do not load if our advanced-cache.php isn't loaded
-if ( ! is_object($batcache) || ! method_exists( $wp_object_cache, 'incr' ) )
-	return;
+class CF_Batcache_Manager {
 
-$batcache->configure_groups();
+	private $_batcache;
 
-// Regen home and permalink on posts and pages
-add_action('clean_post_cache', 'batcache_post');
+	public function __construct($bc) {
+		global $wp_object_cache;
+		// Do not load if our advanced-cache.php isn't loaded
+		if ( ! is_object($bc) || ! method_exists( $wp_object_cache, 'incr' ) ) {
+			return;
+		}
 
-// Regen permalink on comments (TODO)
-//add_action('comment_post',          'batcache_comment');
-//add_action('wp_set_comment_status', 'batcache_comment');
-//add_action('edit_comment',          'batcache_comment');
+		$this->_batcache = $bc;
+		$this->_batcache->configure_groups();
+	}
 
-function batcache_post($post_id) {
-	global $batcache;
+	public function add_hooks() {
+		if ($this->_batcache) {
+			add_action('wp_loaded', array($this, 'wp_loaded'));
+			add_action('admin_bar_menu', array($this, 'admin_bar'), 99);
+			add_action('clean_post_cache', array($this, 'clear_post'));
+		}
+	}
 
-	$post = get_post($post_id);
-	if ( $post->post_type == 'revision' || get_post_status($post_id) != 'publish' )
-		return;
+	public function wp_loaded() {
+		if (is_super_admin() && is_admin()) {
+			if (isset($_GET['flush_cache']) && $_GET['flush_cache'] == 'true') {
+				$this->flush_all();
+				add_action('admin_notices', create_function('', 'echo "<div class=\"updated\"><p>Cache flushed</p></div>";'));
+			}
+		}
+	}
 
-	batcache_clear_url( get_option('home') );
-	batcache_clear_url( trailingslashit( get_option('home') ) );
-	batcache_clear_url( get_permalink($post_id) );
+	public function admin_bar() {
+		global $wp_admin_bar;
+		if ($this->_batcache && is_super_admin() && is_admin()) {
+			$query_string = (is_array($wp->query_string)) ? $wp->query_string : array();
+			$wp_admin_bar->add_menu(array(
+				'id' => 'batcache_flush',
+				'title' => __('Flush Page Cache'),
+				'href' => add_query_arg('flush_cache', 'true', $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]),
+				'meta' => array('onclick' => 'return confirm("Are you sure you want to invalidate caches for the entire site?");'),
+			));
+		}
+	}
+
+	public function flush_all() {
+		global $wp_object_cache;
+		$ret = true;
+		foreach ( array_keys($wp_object_cache->mc) as $group )
+			$ret &= $wp_object_cache->mc[$group]->flush();
+		return $ret;
+	}
+
+	public function clear_home() {
+		$this->clear_url( get_option('home') );
+		$this->clear_url( trailingslashit( get_option('home') ) );
+	}
+
+	public function clear_post($post_id) {
+		$post = get_post($post_id);
+		if ($post->post_type != 'revision' || get_post_status($post_id) == 'publish') {
+			$this->clear_url(get_permalink($post_id));
+			$this->clear_home();
+		}
+	}
+
+	private function clear_url($url) {
+		if (!$url) {
+			return false;
+		}
+		$url_key = md5($url);
+		wp_cache_add("{$url_key}_version", 0, $this->_batcache->group);
+		return wp_cache_incr("{$url_key}_version", 1, $this->_batcache->group);
+	}
 }
 
-function batcache_clear_url($url) {
-	global $batcache;
-	if ( empty($url) )
-		return false;
-	$url_key = md5($url);
-	wp_cache_add("{$url_key}_version", 0, $batcache->group);
-	return wp_cache_incr("{$url_key}_version", 1, $batcache->group);
-}
-
+$cf_batcache = new CF_Batcache_Manager($batcache);
+$cf_batcache->add_hooks();
